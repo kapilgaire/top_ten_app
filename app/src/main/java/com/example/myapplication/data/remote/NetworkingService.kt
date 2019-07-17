749df0297df15f3742305e2e2ee845698a68182b
kapil <kapilgaire2096@gmail.com>
2019-07-17 17:44:00 +0530

    network calling added

---
+++ app/src/main/java/com/example/myapplication/data/remote/NetworkingService.kt
@@ -0,0 +1,8 @@
+package com.example.myapplication.data.remote
+
+import javax.inject.Singleton
+
+@Singleton
+interface NetworkingService {
+
+}
---
+++ app/src/main/java/com/example/myapplication/ui/main/MainViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication.ui.main
+
+import com.example.myapplication.ui.base.BaseViewModel
+import com.example.myapplication.utils.network.NetworkHelper
+
+class MainViewModel(networkHelper: NetworkHelper) : BaseViewModel(networkHelper){
+
+    override fun onCreate() {
+
+    }
+
+}
